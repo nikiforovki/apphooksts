a176bced10e28aeb2c79277397aa201d87e6276b
nikiforovki <143290565+nikiforovki@users.noreply.github.com>
2025-01-31 18:03:20 +0300

    Update useAuth.ts

--- src/app/useAuth.ts
+++ src/app/useAuth.ts
@@ -22,26 +22,4 @@ const useAuth = () => {
 
 export default useAuth;
 
-// import { useEffect } from 'react';
-// import { auth } from '../firebaseConfig';
-// import { onAuthStateChanged } from 'firebase/auth';
 
-// const useAuth = () => {
-//   useEffect(() => {
-//     const unsubscribe = onAuthStateChanged(auth, (user) => {
-//       if (user) {
-//         console.log('Пользователь авторизован:', user);
-//         localStorage.setItem('uid', user.uid);
-//         localStorage.setItem('email', user.email || '');
-//       } else {
-//         console.log('Пользователь не авторизован');
-//         localStorage.removeItem('uid');
-//         localStorage.removeItem('email');
-//       }
-//     });
-
-//     return () => unsubscribe();
-//   }, []);
-// };
-
-// export default useAuth;
